3d704cc31ed328904d05aa7cb3eb9986ba4c761f
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-26 02:14:34 +0530

    Added route.dart file

--- lib/common/route.dart
+++ lib/common/route.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:menukaran/dekstop_copied/desktop_copied.dart';
+import 'package:menukaran/desktop_menu_page/create_desktop.dart';
+import 'package:menukaran/start_page/start_page.dart';
 
-const String homePage = 'homepage';
-const String settingsPage = 'settingspage';
+const String startPage = 'homepage';
+const String createDesktop = 'createdesktop';
+const String desktopCopied = 'desktopCopied';
 Route<dynamic> routecontroller(RouteSettings settings) {
   switch (settings.name) {
-    case homePage:
+    case startPage:
       return MaterialPageRoute(
-        builder: (context) => const HomePage(),
+        builder: (context) => const StartPage(),
       );
-    case settingsPage:
-      return MaterialPageRoute(builder: (context) => const SettingsPage());
+    case createDesktop:
+      return MaterialPageRoute(builder: (context) => const CreateDesktop());
+    case desktopCopied:
+      return MaterialPageRoute(builder: (context) => const DesktopCopied());
     default:
       throw ('No such page!');
   }
