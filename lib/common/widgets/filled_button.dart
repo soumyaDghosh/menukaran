41c3c6161e0cdf4dc229cc821c8d81256e4bcb88
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-27 20:38:47 +0530

    using both filled button & elevated button

--- lib/common/widgets/filled_button.dart
+++ lib/common/widgets/filled_button.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ButtonFilled extends StatelessWidget {
-  const ButtonFilled({super.key, required this.text, this.onPressed});
+  const ButtonFilled(
+      {super.key, required this.text, this.onPressed, required this.elevated});
   final String text;
   final VoidCallback? onPressed;
+  final bool elevated;
 
   @override
   Widget build(BuildContext context) {
-    return FilledButton(
-      onPressed: onPressed ?? () {},
-      style: ButtonStyle(
-        backgroundColor:
-            MaterialStatePropertyAll(Theme.of(context).primaryColor),
-      ),
-      child: Text(text),
-    );
+    return elevated
+        ? ElevatedButton(
+            onPressed: onPressed ?? () {},
+            child: Text(text),
+          )
+        : FilledButton(
+            onPressed: onPressed ?? () {},
+            child: Text(text),
+          );
   }
 }
