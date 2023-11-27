6fb8483350704a28b37527c354d891b90228bdf7
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-27 20:37:03 +0530

    animated icon for greetings

--- lib/start_page/weather_icon.dart
+++ lib/start_page/weather_icon.dart
@@ -1,22 +1,24 @@
 import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:menukaran/common/constants.dart';
 
 class WeatherIcon extends StatelessWidget {
   const WeatherIcon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final iconMap = <TimeOfDay, (IconData, String)>{
-      if (DateTime.now().hour >= 6 && DateTime.now().hour < 12)
-        TimeOfDay.now(): (FeatherIcons.sunrise, 'Good Morning'),
-      if (DateTime.now().hour >= 12 && DateTime.now().hour < 18)
-        TimeOfDay.now(): (FeatherIcons.sun, 'Good Day'),
-      if (DateTime.now().hour >= 18 && DateTime.now().hour < 21)
-        TimeOfDay.now(): (FeatherIcons.sunset, 'Good Evening'),
-      if (DateTime.now().hour >= 21 && DateTime.now().hour < 24)
-        TimeOfDay.now(): (FeatherIcons.moon, 'Good Night'),
-      if (DateTime.now().hour >= 0 && DateTime.now().hour < 6)
-        TimeOfDay.now(): (FeatherIcons.moon, 'Good Night'),
+    final iconMap = <TimeOfDay, (String, String)>{
+      if (DateTime.now().hour >= 5 && DateTime.now().hour < 8)
+        TimeOfDay.now(): ('assets/sun_rise.json', 'Good Morning'),
+      if (DateTime.now().hour >= 8 && DateTime.now().hour < 18)
+        TimeOfDay.now(): ('assets/sun_day.json', 'Good Day'),
+      if (DateTime.now().hour >= 18 && DateTime.now().hour < 20)
+        TimeOfDay.now(): ('assets/moon_rise.json', 'Good Evening'),
+      if (DateTime.now().hour >= 20 && DateTime.now().hour < 24)
+        TimeOfDay.now(): ('assets/moon_night.json', 'Good Night'),
+      if (DateTime.now().hour >= 0 && DateTime.now().hour < 5)
+        TimeOfDay.now(): ('assets/moon_night.json', 'Good Night'),
     };
     return Column(
       children: [
@@ -27,9 +29,16 @@ class WeatherIcon extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        Icon(
-          iconMap[TimeOfDay.now()]?.$1,
-          size: 150,
+        Lottie.asset(
+          iconMap[TimeOfDay.now()]!.$1,
+          height: 150,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 40),
+          child: Text(
+            greeting,
+            style: const TextStyle(fontSize: 15),
+          ),
         ),
       ],
     );
