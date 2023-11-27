cb1d8301cfb55642974dcb98c1674c29284537c4
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-27 20:37:44 +0530

    added more constants

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,15 +1,21 @@
 import 'dart:io';
 
 final userName = Platform.environment['USER'];
-final user = Platform.environment['NAME'];
+final user = Process.runSync(
+  'bash',
+  ["-c", "getent passwd $userName"],
+  runInShell: true,
+).stdout.toString().split(':').elementAt(4).split(',').elementAt(0).toString();
 const appTitle = 'MenuKaran';
-final greeting = 'Hello $user, welcome to $appTitle';
-const filledButton1 = 'Create Desktop';
+final greeting = '$user, welcome to $appTitle';
+const filledButton1 = 'Create Desktop File';
 const desktopFields = {
   'name': ('Desktop Name', 'Mandatory', 1),
   'executable': ('Executable Path', 'Mandatory', 2),
-  'type': ('Type', '', 3),
 };
+
+const types = ['Application', 'Link', 'Directory'];
+final errorMessages = ['Necessary Field: '];
 
 // getent passwd $USER | cut -d : -f 5 | cut -d ', ' -f 1
 // getent passwd $USER | cut -d ':' -f 5 | cut -d ',' -f 1
