47eb731564b1154dcc46a832974723eca96f3ddc
soumyadghosh <soumyadeepghosh2004@zohomail.in>
2023-11-27 20:38:25 +0530

    showing a new page on failure

--- lib/common/route.dart
+++ lib/common/route.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:menukaran/dekstop_copied/desktop_copied.dart';
+import 'package:menukaran/desktop_copy_failed/failed_desktop_copy.dart';
 import 'package:menukaran/desktop_menu_page/create_desktop.dart';
 import 'package:menukaran/start_page/start_page.dart';
 
 const String startPage = 'homepage';
 const String createDesktop = 'createdesktop';
 const String desktopCopied = 'desktopCopied';
+const String failedtoCopy = 'failedtoCopy';
 Route<dynamic> routecontroller(RouteSettings settings) {
   switch (settings.name) {
     case startPage:
@@ -16,6 +18,8 @@ Route<dynamic> routecontroller(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const CreateDesktop());
     case desktopCopied:
       return MaterialPageRoute(builder: (context) => const DesktopCopied());
+    case failedtoCopy:
+      return MaterialPageRoute(builder: (context) => const DesktopCopyFailed());
     default:
       throw ('No such page!');
   }
---
+++ lib/desktop_copy_failed/failed_desktop_copy.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:icon_animated/icon_animated.dart';
+import 'package:menukaran/common/provider.dart';
+import 'package:menukaran/common/widgets/header.dart';
+import 'package:provider/provider.dart';
+import 'package:yaru_icons/yaru_icons.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
+
+class DesktopCopyFailed extends StatefulWidget {
+  const DesktopCopyFailed({super.key});
+  @override
+  State<DesktopCopyFailed> createState() => _DesktopCopyFailedState();
+}
+
+class _DesktopCopyFailedState extends State<DesktopCopyFailed> {
+  @override
+  Widget build(BuildContext context) {
+    final text = context.read<ValueProvider>().message;
+    return Scaffold(
+      appBar: const TitleBar(
+        leading: YaruBackButton(),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const IconAnimated(
+              active: true,
+              size: 200,
+              iconType: IconType.error,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                child: YaruBanner(
+                  color: Colors.black,
+                  padding: const EdgeInsets.all(20),
+                  child: Flexible(
+                    child: SingleChildScrollView(
+                      child: SelectableText(
+                        text,
+                        style: TextStyle(
+                          fontFamily: 'Ubuntu Mono',
+                          color: Colors.white,
+                          fontSize:
+                              Theme.of(context).textTheme.bodyLarge!.fontSize,
+                          textBaseline: TextBaseline.alphabetic,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
